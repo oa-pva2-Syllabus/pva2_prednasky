e3b32c4145604d9cbc7d0da0d84b11ca74b3c041
Adam Fišer <adam.fiser@wanex.cz>
2024-05-21 13:29:23 +0200

    split build and build-index

--- scripts/build.js
+++ scripts/build.js
@@ -29,39 +29,3 @@ for (const file of talksFiles) {
     }
   }
 }
-
-
-
-console.log( "📃 build index ...");
-for (const file of talksFiles) {
-  if (file.isDirectory()) {
-    try {
-      const rawPackageJson = await fs.readFile(path.join(file.path, file.name, 'src/package.json'));
-      const packageJson = JSON.parse(rawPackageJson.toString());
-      talks.push({path: file.name, title: packageJson.title, author: packageJson.author});
-    } catch (err) {
-      console.error(err);
-    }
-  }
-}
-
-const createLinkList = (talks) => talks.map(talk => `<li><a href="./${talk.path}">${talk.title} by ${talk.author.name}</a></li>`).join('');
-await fs.writeFile(path.join(OUTPUT_PATH, 'index.html'), `<!DOCTYPE html>
-<html lang="cs">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Our internal talks</title>
-    <link rel="stylesheet" href="styles.css" />
-  </head>
-  <body>
-    <img src="https://www.wanex.cz/wp-content/uploads/2013/01/logo.png" alt="Wanex logo" />
-    <h1>Přednášky</h1>
-    <ul>
-      ${createLinkList(talks.reverse())}
-    </ul>
-  </body>
-</html>
-`);
-
-await fs.copyFile(path.join(PUBLIC_PATH, 'styles.css'), path.join(OUTPUT_PATH, 'styles.css'));
