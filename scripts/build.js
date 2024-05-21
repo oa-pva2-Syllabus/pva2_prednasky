5a30494b04820cf94e518fc29a43d6698b2b83de
Adam Fišer <adam.fiser@wanex.cz>
2024-05-21 07:44:29 +0200

    Fix build and deploy

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,9 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
 const TALKS_PATH = 'pva2/';
 const OUTPUT_PATH = 'dist';
 const PUBLIC_PATH = 'public/';
@@ -8,10 +11,28 @@ const talks = [];
 
 const talksFiles = await fs.readdir(TALKS_PATH, { withFileTypes: true });
 
+const execPromise = promisify(exec);
+
+console.log( "📃 build slides ...");
+for (const file of talksFiles) {
+
+  if (file.isDirectory()) {
+    try {
+      const rawPackageJson = await fs.readFile(path.join(file.path, file.name, 'src/package.json'));
+      const packageJson = JSON.parse(rawPackageJson.toString());
+
+      console.log(packageJson.name);
+      await execPromise(`pnpm -r --filter=./pva2/${packageJson.name}/src/** run build`);
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
 
 
 
-
+console.log( "📃 build index ...");
 for (const file of talksFiles) {
   if (file.isDirectory()) {
     try {
