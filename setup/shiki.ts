afb4b1dc7ec160f144b1bc29f6a9201996bc6d29
Adam Fišer <adam.fiser@wanex.cz>
2024-10-05 08:03:47 +0200

    feat: Default konfigurace stylu `shiki`

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,14 +1,10 @@
-/* ./setup/shiki.ts */
 import { defineShikiSetup } from '@slidev/types'
 
 export default defineShikiSetup(() => {
   return {
     themes: {
       dark: 'github-dark-default', /*'min-dark',*/
-      light: 'github-light-default', /*'min-light'*/
+      light: 'github-light', /*'min-light'*/
     },
-    transformers: [
-      // ...
-    ],
   }
 })
